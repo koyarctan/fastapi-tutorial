c612ffbf56994a96d90b7e09199873f5a42eaab7
koyarctan <koya@veldt.jp>
2026-05-14 10:30:23 +0900

    query parameter

--- main.py
+++ main.py
@@ -21,3 +21,31 @@ class ModelName(str, Enum):
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     return {"model_name": model_name}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+#複数のパスパラとクエパラ
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+#パスパラ、クエパラ、必須クエパラ
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
